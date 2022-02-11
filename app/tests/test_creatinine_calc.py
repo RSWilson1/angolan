d9351976efc71ac3e704f84da6726ae85b829691
rachaelaye <rachael.ayegba@postgrad.manchester.ac.uk>
2022-02-11 10:21:32 +0000

    fixed error message when radio button not checked functionality

--- app/tests/test_creatinine_calc.py
+++ app/tests/test_creatinine_calc.py
@@ -4,19 +4,22 @@ class TestInputSex(unittest.TestCase):
     def test_sex_is_selected(self):
         """Link to the form or website to test and test with dummy data values."""
 
-    def test_age_maximum(self):
+class TestAgeInput(unittest.TestCase):
+    def test_performCalc(self):
         """Test that the input age is not above maximum"""
-        self.assertGreater(self., 0, "Check that the average in not negative.")
+        self.assertGreater(self.performCalc(), 0, "Check that the average in not negative.")
 
     def test_age_minimum(self):
         """Test that the input age is not below minimum """
 
+class TestWeightInput(unittest.TestCase):
     def test_weight_maximum(self):
         """Test that the input weight is not above maximum"""
 
     def test_weight_minimum(self):
         """Test that the input weight is not below minimum"""
 
+class TestCreatInput(unittest.TestCase):
     def test_creatinine_maximum(self):
         """Test that the input serum creatinine is not above maximum"""
 
